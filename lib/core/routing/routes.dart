8e6ffd419e65e815ca5617f8f1069e6beee0bbfd
AbdllahMohamed <dev.abdo2000@gmail.com>
2024-09-07 22:47:28 +0300

    add login integration with api

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
   static const String onBardingScreen = '/onBardingScreen';
   static const String loginScreen = '/loginScreen';
+  static const String homeScreen = '/homeScreen';
 }
